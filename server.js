263b5b32140ea57cb4a5cd256be0d7adeda07606
Huangxing Chen <xingye1031@gmail.com>
2024-04-18 15:08:25 -0400

    update server.js

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 import express from "express";
 import cors from "cors";
+import path from "path";
 // import records from "./routes/dbRecords.js";
 
 const PORT = process.env.PORT || 5050;
 const app = express();
+
 
 app.use(cors());
 app.use(express.json());
@@ -13,7 +15,7 @@ app.use(express.json());
 
 app.set('port', PORT);
 
-app.use(express.static('client/build'));
+app.use(express.static('./client/build'));
 
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "client", "build",     
