51f6f32c85ad4e2088114b2f88ec8398d6b8f5eb
Huangxing Chen <xingye1031@gmail.com>
2024-04-18 19:22:10 -0400

    1

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.json());
 
 app.set('port', PORT);
 
-app.use(express.static('./client/build'));
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 app.get("*", (req, res) => {
   console.log(req)
